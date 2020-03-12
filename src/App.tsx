b29c44b1559d3bbba6039d2645a33f2daf9a223b
Matroskin94 <anton1994anton@mail.ru>
2020-03-12 14:12:15 +0300

    [POLIGON-1] - Добавление плагина для eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+import {Root} from './components/root/root';
+
 ReactDOM.render(
-    <h1>Hello TS</h1>,
+    <Root />,
     document.getElementById('app')
 );
